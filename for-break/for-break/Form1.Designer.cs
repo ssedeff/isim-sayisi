33156e785a273b2deb15f6cc14675a9d742c9240
ssedeff <145463803+ssedeff@users.noreply.github.com>
2023-11-20 11:22:23 +0300

    Add files via upload

---
+++ for-break/for-break/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace for_break
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAranan = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAranan
+            // 
+            this.btnAranan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAranan.Location = new System.Drawing.Point(208, 126);
+            this.btnAranan.Name = "btnAranan";
+            this.btnAranan.Size = new System.Drawing.Size(100, 32);
+            this.btnAranan.TabIndex = 0;
+            this.btnAranan.Text = "aranan";
+            this.btnAranan.UseVisualStyleBackColor = true;
+            this.btnAranan.Click += new System.EventHandler(this.aranan_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "onur",
+            "yankı",
+            "elif",
+            "hira",
+            "rabia",
+            "hasan",
+            "berk",
+            "yavuz",
+            "yahya",
+            "samet",
+            "yaren",
+            "rümeysa",
+            "burak",
+            "ateş",
+            "dünya"});
+            this.listBox1.Location = new System.Drawing.Point(49, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(208, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnAranan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAranan;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ for-break/for-break/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_break
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aranan_Click(object sender, EventArgs e)
+        {
+            string aranan = textBox1.Text;
+
+            int sayac = 0;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (aranan == listBox1.Items[i].ToString())
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 0)
+            {
+                MessageBox.Show("öğrenci bulunamadı.");
+            }
+            else
+            {
+                MessageBox.Show(sayac + "öğrenci bulundu.");
+            }
+
+        }
+    }
+}
